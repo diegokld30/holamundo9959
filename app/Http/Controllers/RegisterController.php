61b094c054705024c77f4adab4821c18ce4aa47a
diegokld30 <dfercalderon@poligran.edu.co>
2023-11-15 16:37:53 -0500

    se cambio el lenguaje de los errores y se creo la migracion para la columna username

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,14 +11,15 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request){
-        //dd($request->password);
         //Vamos a crear el proceso de validacion
         $this->validate($request,[
             'name'=>'required|min:3|max:50',
-            'username'=>'required|min:3|max:50',
-            'email'=>'required',
-            'password'=>'required'
+            'username'=>'required|unique:users|min:3|max:50',
+            'email'=>'required|unique:users|email|max:60',
+            'password'=>'required|confirmed'
 
         ]);
+
+        dd("creando el usuario");
     }
 }
